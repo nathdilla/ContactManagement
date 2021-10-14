ab47c8d1d73a2b55ccd066c605c2f0496a7b9a96
Nathan Dilla <nathcdilla@protonmail.com>
2021-10-13 18:13:16 -0700

    Initial Commit

---
+++ ContactManagement/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ContactManagement
+//
+//  Created by Nathan Dilla on 10/13/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
